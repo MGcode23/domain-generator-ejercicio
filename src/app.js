aed2c43b68e88ed3a5257d4aab86b603e9ee23ed
MGcode23 <marcrudo23@gmail.com>
2023-02-21 23:57:25 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#name").innerHTML = domainName();
+  });
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function domainName() {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    let temp = "";
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          temp += pronoun[i] + adj[j] + noun[k] + ".com <br>";
+        }
+      }
+    }
+    return temp;
+  }
 };
